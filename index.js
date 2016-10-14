00188eab3b26d9d0d4a4b2e0fe09089a429c82a8
Dan Hayden <pythonic1@gmail.com>
2016-10-14 11:10:20 +0100

    fix: when the request errors only attempt to send response if it is available

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var lookup = function(bin){
   var deffered = q.defer();
   request.get('https://bins.payout.com/api/v1/bins/'+bin).end(function(err, response){
     if(err){
-      deffered.reject(JSON.parse(response.text));
+      if(response){
+        deffered.reject(JSON.parse(response.text));
+      } else {
+        deffered.resolve(JSON.parse(err));
+      }
     } else {
-      deffered.resolve(JSON.parse(response.text))
+      deffered.resolve(JSON.parse(response.text));
     }
   })
   return deffered.promise;
